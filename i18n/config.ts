cc036cc12e8782af883dc645268c2b211a61ae61
Juanjho120 <juan.jose120@hotmail.com>
2026-06-26 10:04:57 -0600

    en default language

--- i18n/config.ts
+++ i18n/config.ts
@@ -1,8 +1,8 @@
-export const locales = ["es", "en"] as const;
+export const locales = ["en", "es"] as const;
 
 export type Locale = (typeof locales)[number];
 
-export const defaultLocale: Locale = "es";
+export const defaultLocale: Locale = "en";
 
 export function isLocale(value: string): value is Locale {
   return locales.includes(value as Locale);
